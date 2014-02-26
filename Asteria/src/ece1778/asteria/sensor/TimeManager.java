9cf398789b3a9dd91f06953022c8fde3dadcf105
Arjun <Arjun@Arjun-PC>
2014-02-25 22:08:52 -0500

    conversion to days

--- Asteria/src/ece1778/asteria/sensor/TimeManager.java
+++ Asteria/src/ece1778/asteria/sensor/TimeManager.java
@@ -24,8 +24,52 @@ public class TimeManager {
 			J2000Time = 1.0;
 			return J2000Time;
 		}
-		J2000Time = millitime - MS_BETWEEN_ORIGINAL_EPOCH_AND_Y2K_EPOCH;
+		J2000Time = (millitime - MS_BETWEEN_ORIGINAL_EPOCH_AND_Y2K_EPOCH);
+		Double inmilis= J2000Time;
+		J2000Time = getDuration((inmilis.toString()));
 		return J2000Time;
 	}
+	@SuppressWarnings("finally")
+	public double getDuration(String _currentTimemilliSecond)
+    {
+        long _currentTimeMiles = 1;         
+        int x = 0;
+        int seconds = 0;
+        int minutes = 0;
+        int hours = 0;
+        double days = 0;
+      
 
+        try 
+        {
+            _currentTimeMiles = Long.parseLong(_currentTimemilliSecond);
+            /**  x in seconds **/   
+            x = (int) (_currentTimeMiles / 1000) ; 
+            seconds = x ;
+
+            if(seconds >59)
+            {
+                minutes = seconds/60 ;
+
+                if(minutes > 59)
+                {
+                    hours = minutes/60;
+
+                    if(hours > 23)
+                    {
+                        days = hours/24 ;
+                    }
+                }
+           
+                return days;}
+        }
+        
+        catch (Exception e)
+        {
+    }
+finally
+{
+	return days;
+}
+}
 }
